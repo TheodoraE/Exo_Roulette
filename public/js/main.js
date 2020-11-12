a116482085bb1bf5c31107b10f80de3b26fd8629
TheodoraE <tidoraa@gmail.com>
2020-11-12 19:53:38 +0100

    two

--- public/js/main.js
+++ public/js/main.js
@@ -2,28 +2,35 @@
 // condition = true;
 let argent = 0;
 let roulette;
-let couleurs = ["Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir","Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Noir", "Rouge", "Rouge", "Rouge", "Rouge", "Rouge", "Rouge", "Rouge", "Rouge","Rouge", "Rouge", "Rouge","Rouge", "Rouge", "Rouge","Rouge","Rouge", "Rouge", "Rouge", "Vert"];
+let couleurs = ["Vert", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Rouge", "Noir", "Rouge", "Noir","Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge", "Noir", "Rouge"];
 let ranC;
+
+alert(`   ~~~ Bonjour et bienvenue au jeu de la roulette ! ~~~`);
+
+alert(`Voici les règles du jeu : Choisissez une couleur sur laquelle vous souhaitez miser.
+À chaque bon tirage, vous recevez 2 points et vous pourrez continuer la partie jusqu'à ce que vous n'ayez plus de points. Par contre, lorsque votre mise n'est pas bonne, vous perdez 2 points.
+ATTENTION: Vous risquez de tomber sur le chiffre vert 0. Dans ce cas, vous perdez tous vos gains.`);
 
 
 do {
-    roulette = prompt(`Sur quelle couleur misez-vous :
-            Rouge ou Noir ?`).toLowerCase();
-    ranC = Math.round(Math.random() * (couleurs.length -1));
-            console.log(ranC);
+    roulette = prompt(`Sur quelle couleur misez-vous?
+    Veuillez entre une valeur entre Rouge et Noir ?`).toLowerCase();
+    ranC = Math.round(Math.random() * (couleurs.length +1));
+            console.log(ranC); // CHEAT
     if ((roulette.charAt(0).toUpperCase() + roulette.substr(1)) == couleurs[ranC]){
-        console.log("Bravo, c'est votre jour de chance ! 2 points ont été crédités à votre compte");
+        alert("Bravo, c'est votre jour de chance ! 2 points ont été crédités à votre compte");
         argent+=2;
+    } else if (("vert" || "Vert") == couleurs[ranC]) {
+        alert("Vous êtes tombé sur le chiffre vert 0. Vous avez tout perdu... T.T");
+        argent == 0;
     } else {
-        console.log("Ah c'est dommage, peut-être la prochaine fois...");
+        alert("Ah c'est dommage... 2 points ont été débités de votre compte");
         argent-=2;
     };
-    console.log(`Votre solde est de ${argent} points`)
+    alert(`Votre solde est de ${argent} points`);
 } while (argent > 0) {
-    console.log("Tu n'as plus de points, t'es dans la merte...");
+    alert("Tu n'as plus de points, le jeu est FINI");
 };
-
-
 
 
 
